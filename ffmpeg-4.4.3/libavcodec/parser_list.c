df0d9e7a6695479ffa740e46bcbe8afb9a4a909f
VinterYoung5 <343196469@qq.com>
2022-10-19 11:12:51 +0800

    init

---
+++ ffmpeg-4.4.3/libavcodec/parser_list.c
@@ -0,0 +1,10 @@
+static const AVCodecParser * const parser_list[] = {
+    &ff_aac_latm_parser,
+    &ff_ac3_parser,
+    &ff_av1_parser,
+    &ff_flac_parser,
+    &ff_h263_parser,
+    &ff_mpeg4video_parser,
+    &ff_mpegaudio_parser,
+    &ff_vp9_parser,
+    NULL };
